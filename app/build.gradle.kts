f9de3f7dc6e00b15d4c5df930178ee9b727c3724
Anton Tananaev <anton.tananaev@gmail.com>
2026-03-09 17:23:46 -0700

    Update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "org.traccar.relay"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
 }
 
