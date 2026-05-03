a71d8dca79d03b03f2c5fde662ffbd69456ff015
Anton Tananaev <anton.tananaev@gmail.com>
2026-05-03 14:49:00 -0700

    Request battery optimization exception

--- app/src/main/java/org/traccar/relay/MainActivity.kt
+++ app/src/main/java/org/traccar/relay/MainActivity.kt
@@ -1,12 +1,18 @@
 package org.traccar.relay
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.PowerManager
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.core.content.getSystemService
 import androidx.lifecycle.viewmodel.compose.viewModel
 import org.traccar.relay.auth.KeySetupScreen
 import org.traccar.relay.auth.LoginScreen
@@ -15,6 +21,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        requestBatteryOptimizationExemption()
         setContent {
             MaterialTheme {
                 val viewModel: MainViewModel = viewModel()
@@ -28,6 +35,19 @@ class MainActivity : ComponentActivity() {
                     DeviceListScreen(viewModel)
                 }
             }
+        }
+    }
+
+    @SuppressLint("BatteryLife")
+    private fun requestBatteryOptimizationExemption() {
+        val powerManager: PowerManager = getSystemService() ?: return
+        if (powerManager.isIgnoringBatteryOptimizations(packageName)) return
+        try {
+            startActivity(
+                Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)
+                    .setData(Uri.parse("package:$packageName"))
+            )
+        } catch (_: Exception) {
         }
     }
 }
